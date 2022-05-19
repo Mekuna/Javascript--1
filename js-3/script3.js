581e51580437b92cc94b0c4b71af9a4bd306993e
Meerim Abdyrahmanovna <87778832+Mekuna@users.noreply.github.com>
2022-05-19 16:27:17 +0600

    Add files
    Add JS homeworks

---
+++ js-3/script3.js
@@ -0,0 +1,5 @@
+
+/*Напишите несколько строк кода, выводящих на экран ваше имя и почтовый адрес.*/
+let userName = prompt("Firstname");
+let userEmail = prompt("Email");
+document.write("My name is: " + userName + "<br>" + "My Email: " + userEmail)
